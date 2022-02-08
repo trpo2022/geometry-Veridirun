b962fc1e52448163a8f0fbf4b99081e2326a4d12
Kirill Gaponov <28047836+Veridirun@users.noreply.github.com>
2022-02-09 00:33:29 +0700

    Test commit to try revert changes

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World!");
+    int length = 5;
+    printf("\n");
+
+    while (length < 21) {
+        length++;
+        if (length > 20)
+            printf("strashno\n");
+    }
     return 0;
 }
 
