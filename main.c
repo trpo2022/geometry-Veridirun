09d2f8d42f4121e5186b720a8c651561a322d7d9
Kirill Gaponov <28047836+Veridirun@users.noreply.github.com>
2022-02-20 23:52:54 +0700

    Add reading line from lab_input.txt

--- main.c
+++ main.c
@@ -1,64 +1,183 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+//#define M_PI 3.1415
 #define SIZE_STR 100
 
-float OutputCirclePerim(float r){
-	return 2*M_PI*r;
+float OutputCirclePerim(float r)
+{
+    return 2 * M_PI * r;
 }
 
-float OutputCircleSq(float r){
-	return M_PI*powf(r,2);
+float OutputCircleSq(float r)
+{
+    return M_PI * powf(r, 2);
 }
 
-float OutputTriangleSq(float x11, float x12, float x21, float x22, float x31, float x32){
-	float vector_a1=x21-x11;
-	float vector_a2=x22-x12;
-	float vector_b1=x31-x11;
-	float vector_b2=x32-x12;
-	float length_a=sqrt(powf(vector_a1,2)+powf(vector_a2,2));
-	float length_b=sqrt(powf(vector_b1,2)+powf(vector_b2,2));
-	float cos_ab=(vector_a1*vector_b1+vector_a2*vector_b2)/(length_a*length_b);
-	float sin_ab=sqrt(1-powf(cos_ab,2));
-	return length_a*length_b*sin_ab/2;
+float OutputTriangleSq(
+        float x11, float x12, float x21, float x22, float x31, float x32)
+{
+    float vector_a1 = x21 - x11;
+    float vector_a2 = x22 - x12;
+    float vector_b1 = x31 - x11;
+    float vector_b2 = x32 - x12;
+    float length_a = sqrt(powf(vector_a1, 2) + powf(vector_a2, 2));
+    float length_b = sqrt(powf(vector_b1, 2) + powf(vector_b2, 2));
+    float cos_ab = (vector_a1 * vector_b1 + vector_a2 * vector_b2)
+            / (length_a * length_b);
+    float sin_ab = sqrt(1 - powf(cos_ab, 2));
+    return length_a * length_b * sin_ab / 2;
 }
 
 int main()
 {
-	FILE *file;
-	char *fname = "lab_input.txt";//РЅР°Р·РІР°РЅРёРµ С„Р°Р№Р»Р°
-	file = fopen(fname,"r");//РѕС‚РєСЂС‹РІР°РµРј С„Р°Р№Р»
+    FILE* file;
+    char* fname = "lab_input.txt"; //название файла
+    file = fopen(fname, "r");      //открываем файл
 
-	int c[SIZE_STR];//РѕР±СЉСЏРІР»СЏРµРј РјР°СЃСЃРёРІ СЃРёРјРІРѕР»РѕРІ
-	int i=0;
+    int C[SIZE_STR]; //объявляем массив символов
+    int i = 0;
+    int iL, iD = 0, iR;
+    float x_circle = 0, y_circle = 0, radius = 0;
 
-	if (file == NULL)//РµСЃР»Рё РѕС‚РєСЂС‹С‚СЊ РЅРµ РїРѕР»СѓС‡РёР»РѕСЃСЊ, С‚Рѕ Р·Р°РєСЂС‹РІР°РµРјСЃСЏ
-	{
-		printf("unable to open file '%s'", fname);
-		return 0;
-	}
+    if (file == NULL) //если открыть не получилось, то закрываемся
+    {
+        printf("unable to open file '%s'", fname);
+        return 0;
+    }
 
-	c[i]=fgetc(file); //СЃС‡РёС‚С‹РІР°РµРј РїРµСЂРІС‹Р№ СЃРёРјРІРѕР» РІ c[0]
+    C[i] = fgetc(file); //считываем первый символ в c[0]
 
-	while (c[i] != EOF) //РїРѕРєР° РЅРµ РІСЃС‚СЂРµС‚РёРј РєРѕРЅРµС† С„Р°Р№Р»Р°, РІС‹РїРѕР»РЅСЏРµРј С†РёРєР»
-	{
-		i++;
-		c[i]=fgetc(file);//РІ РѕС‡РµСЂРµРґРЅРѕР№ СЌР»РµРјРµРЅС‚ РјР°СЃСЃРёРІР° РїСЂРёСЃРІР°РёРІР°РµРј ascii-РєРѕРґ СЃРёРјРІРѕР»Р°
-	}
-	i = 0;
+    while (C[i] != EOF) //пока не встретим конец файла, выполняем цикл
+    {
+        i++;
+        C[i] = fgetc(file); //в очередной элемент массива присваиваем ascii-код
+                            //символа
+    }
 
-	while (c[i]!=EOF)//РІС‹РІРѕРґ РјР°СЃСЃРёРІР°
-	{
-		printf("%c",c[i]);
-		i++;
-	}
+    for (i = 0; C[i] == ' '; i++)
+        ;
 
+    if ((C[i] == 'c' || C[i] == 'C') && (C[i + 1] == 'i' || C[i + 1] == 'I')
+        && (C[i + 2] == 'r' || C[i + 2] == 'R')
+        && (C[i + 3] == 'c' || C[i + 3] == 'C')
+        && (C[i + 4] == 'l' || C[i + 4] == 'L')
+        && (C[i + 5] == 'e' || C[i + 5] == 'E')
+        && (C[i + 6] == ' ' || C[i + 6] == '(')) {
+        //		printf("word circle is correct\n");
+        i += 6;
+    } else {
+        printf("word circle is not correct\n");
+        return 1;
+    }
 
+    for (; C[i] == ' '; i++)
+        ;
+    if ((C[i] == '(' && C[i + 1] == '(')) {
+        i += 2;
+    } else {
+        printf("opening brackets are not correct\n");
+    }
 
+    for (; C[i] == ' '; i++)
+        ;
+    iL = i;
+    for (; C[i] != ' '; i++) { // start reading x_circle
+        if (C[i + 1] == '.') {
+            iD = i + 1;
+        }
+    }
+    iR = i;
+    i--;
+    //	printf("iL=%d iD=%d iR=%d i=%d C[i]=%c\n",iL, iD, iR, i, C[i]);
 
-	//printf("%f   %f\n", OutputCirclePerim(3),OutputCircleSq(3));
-	//printf("%f", OutputTriangleSq(2,1,3,4,5,2));
-	return 0;
-    
+    for (; (i != iD) && iD != 0; i--) {
+        x_circle += powf(10, -(i - iD)) * (C[i] - 48);
+    }
+    for (i--; i != iL - 1; i--) {
+        x_circle += pow(10, (iD - i - 1)) * (C[i] - 48);
+    }
+    //	printf("x_circle=%f\n", x_circle);
+    i = iR;
+    iD = 0;
+
+    for (; C[i] == ' '; i++)
+        ;
+    iL = i;
+    for (; (C[i] != ' ') && (C[i] != ',') && (C[i] != ')');
+         i++) { // start reading y_circle
+        if (C[i + 1] == '.') {
+            iD = i + 1;
+        }
+    }
+    iR = i;
+    i--;
+    //	printf("iL=%d iD=%d iR=%d i=%d C[i]=%c\n",iL, iD, iR, i, C[i]);
+
+    for (; (i != iD) && iD != 0; i--) {
+        y_circle += powf(10, -(i - iD)) * (C[i] - 48);
+    }
+    for (i--; i != iL - 1; i--) {
+        y_circle += pow(10, (iD - i - 1)) * (C[i] - 48);
+    }
+    //	printf("y_circle=%f\n", y_circle);
+    i = iR;
+    iD = 0;
+
+    for (; C[i] == ' '; i++)
+        ;
+    if (C[i] == ',')
+        i++;
+    else {
+        printf("expected comma");
+        return 2;
+    }
+    for (; C[i] == ' '; i++)
+        ;
+    iL = i;
+    for (; (C[i] != ' ') && (C[i] != ',') && (C[i] != ')');
+         i++) { // start reading radius
+        if (C[i + 1] == '.') {
+            iD = i + 1;
+        }
+    }
+    iR = i;
+    i--;
+    //	printf("iL=%d iD=%d iR=%d i=%d C[i]=%c\n",iL, iD, iR, i, C[i]);
+    for (; (i != iD) && iD != 0; i--) {
+        radius += powf(10, -(i - iD)) * (C[i] - 48);
+    }
+    for (i--; i != iL - 1; i--) {
+        radius += pow(10, (iD - i - 1)) * (C[i] - 48);
+    }
+    //	printf("radius=%f\n", radius);
+    i = iR;
+
+    for (; C[i] == ' '; i++)
+        ;
+    //	printf("iL=%d iD=%d iR=%d i=%d C[i]=%c\n",iL, iD, iR, i, C[i]);
+    if (C[i] == ')' && C[i + 1] == ')') {
+        //		printf("line should be correct\n");
+        i += 2;
+    } else {
+        printf("closing brackets are not correct");
+        return 3;
+    }
+
+    while (C[i] != EOF) //вывод оставшегося массива (ничего не должно остаться)
+    {
+        printf("%c", C[i]);
+        i++;
+    }
+
+    printf("circle(%f %f, %f)",
+           x_circle,
+           y_circle,
+           radius); //есть проблема, что появляются погрешности в числах с
+                    //плавающей точкой
+
+    // printf("%f   %f\n", OutputCirclePerim(3),OutputCircleSq(3));
+    // printf("%f", OutputTriangleSq(2,1,3,4,5,2));
+    return 0;
 }
 
