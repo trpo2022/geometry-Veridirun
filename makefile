faec9439212119e1c262110506c2d9c1552a7c05
Kirill Gaponov <28047836+Veridirun@users.noreply.github.com>
2022-05-01 19:06:39 +0700

    Move from c to c++

--- makefile
+++ makefile
@@ -13,7 +13,7 @@ SRC_DIR = src
 APP_PATH = $(BIN_DIR)/$(APP_NAME)
 LIB_PATH = $(OBJ_DIR)/$(SRC_DIR)/$(LIB_NAME)/$(LIB_NAME).a
 
-SRC_EXT = c
+SRC_EXT = cpp
 
 APP_SOURCES = $(shell find $(SRC_DIR)/$(APP_NAME) -name '*.$(SRC_EXT)')
 APP_OBJECTS = $(APP_SOURCES:$(SRC_DIR)/%.$(SRC_EXT)=$(OBJ_DIR)/$(SRC_DIR)/%.o)
@@ -34,7 +34,7 @@ $(APP_PATH): $(APP_OBJECTS) $(LIB_PATH)
 $(LIB_PATH): $(LIB_OBJECTS)
 	ar rcs $@ $^
 
-$(OBJ_DIR)/%.o: %.c
+$(OBJ_DIR)/%.o: %.cpp
 	$(CC) -c $(CFLAGS) $(CPPFLAGS) $< -o $@
 
 .PHONY: clean
---
+++ src/geometry/main.cpp
@@ -0,0 +1,25 @@
+#include "libgeometry/geom_string.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE_STR 100
+
+float OutputTriangleSq(
+        float x11, float x12, float x21, float x22, float x31, float x32)
+{
+    float vector_a1 = x21 - x11;
+    float vector_a2 = x22 - x12;
+    float vector_b1 = x31 - x11;
+    float vector_b2 = x32 - x12;
+    float length_a = sqrt(powf(vector_a1, 2) + powf(vector_a2, 2));
+    float length_b = sqrt(powf(vector_b1, 2) + powf(vector_b2, 2));
+    float cos_ab = (vector_a1 * vector_b1 + vector_a2 * vector_b2)
+            / (length_a * length_b);
+    float sin_ab = sqrt(1 - powf(cos_ab, 2));
+    return length_a * length_b * sin_ab / 2;
+}
+
+int main()
+{
+    return ProcessGeomFile();
+}
