7b3ccc130a4cc654454b26a5602fce5a3023ef98
Kirill Gaponov <28047836+Veridirun@users.noreply.github.com>
2022-05-08 12:31:00 +0700

    Making program from beginning

--- src/libgeometry/geom_string.cpp
+++ src/libgeometry/geom_string.cpp
@@ -13,150 +13,123 @@ float OutputCircleArea(float r)
     return ((float)M_PI * powf(r, 2));
 }
 
-void SkipSpaces(int C[], int* i)
+void SkipSpaces(char C[], int &i)
 {
-    for (; C[*i] == ' '; ++*i)
+    for (; C[i] == ' '; ++i)
         ;
 }
 
-int ProcessGeomFile()
-{
-    FILE* file;
-    char fname[] = "lab_input.txt";
-    file = fopen(fname, "r");
+int CountLines(FILE* file){
+	char c;
+	int lineAmount=1;
+	
+	c=getc(file);
+	if(c==EOF) 
+		return 0;
+	while(c!=EOF){
+		if(c=='\n')
+			lineAmount++;
+		c=getc(file);
+	}
+	return lineAmount;
+}
 
-    int C[SIZE_STR];
-    char circleName[] = "circle";
-    int i = 0;
-    int iL, iD = 0, iR;
-    float x_circle = 0, y_circle = 0, radius = 0;
+void AllocateMemory(char **&C, int lineAmount){ //allocate memory to array of chars for every line
+	C=new char*[lineAmount]; 
+	int i;
+	for(i=0;i<lineAmount;i++){
+		C[i]=new char[SIZE_STR];
+	}
+}
 
-    if (file == NULL) {
-        printf("unable to open file '%s'", fname);
-        return 10;
-    }
+void ReadLines(FILE *file, char **&C, int lineAmount){
+	int i,j;
+	rewind(file);
+	char ch=getc(file);
+	for(i=0;i<lineAmount;i++){
+		for(j=0;ch!='\n'&&ch!=EOF;j++){
+			C[i][j]=ch;
+			ch=getc(file);
+		}
+		ch=getc(file);
+		C[i][j]=0;
+	}
+}
 
-    C[i] = fgetc(file);
-
-    while (C[i] != EOF) {
-        i++;
-        C[i] = fgetc(file);
-    }
-    i = 0;
-
-    SkipSpaces(C, &i);
-
-    if ((C[i] == 'c' || C[i] == 'C') && (C[i + 1] == 'i' || C[i + 1] == 'I')
+int CheckWord(char *&C, int &i){
+    if ((C[i] == 'c' || C[i] == 'C') 
+		&& (C[i + 1] == 'i' || C[i + 1] == 'I')
         && (C[i + 2] == 'r' || C[i + 2] == 'R')
         && (C[i + 3] == 'c' || C[i + 3] == 'C')
         && (C[i + 4] == 'l' || C[i + 4] == 'L')
         && (C[i + 5] == 'e' || C[i + 5] == 'E')
         && (C[i + 6] == ' ' || C[i + 6] == '(')) {
         i += 6;
+        return 0;
     } else {
-        printf("word circle is not correct\n");
         return 1;
-    }
-
-    SkipSpaces(C, &i);
-
-    if ((C[i] == '(')) {
-        i++;
-    } else {
-        printf("opening brackets are not correct\n");
-        return 4;
-    }
-
-    SkipSpaces(C, &i);
-
-    iL = i;
-    for (; C[i] != ' '; i++) { // start reading x_circle
-        if (C[i + 1] == '.') {
-            iD = i + 1;
-        }
-    }
-    iR = i;
-    i--;
-
-    for (; (i != iD) && iD != 0; i--) {
-        x_circle += powf(10, -(i - iD)) * (C[i] - 48);
-    }
-    for (i--; i != iL - 1; i--) {
-        x_circle += pow(10, (iD - i - 1)) * (C[i] - 48);
-    }
-
-    i = iR;
-    iD = 0;
-
-    SkipSpaces(C, &i);
-
-    iL = i;
-    for (; (C[i] != ' ') && (C[i] != ',') && (C[i] != ')');
-         i++) { // start reading y_circle
-        if (C[i + 1] == '.') {
-            iD = i + 1;
-        }
-    }
-    iR = i;
-    i--;
-
-    for (; (i != iD) && iD != 0; i--) {
-        y_circle += powf(10, -(i - iD)) * (C[i] - 48);
-    }
-    for (i--; i != iL - 1; i--) {
-        y_circle += pow(10, (iD - i - 1)) * (C[i] - 48);
-    }
-    i = iR;
-    iD = 0;
-
-    for (; C[i] == ' '; i++)
-        ;
-    if (C[i] == ',')
-        i++;
-    else {
-        printf("expected comma");
-        return 2;
-    }
-    for (; C[i] == ' '; i++)
-        ;
-    iL = i;
-    for (; (C[i] != ' ') && (C[i] != ',') && (C[i] != ')');
-         i++) { // start reading radius
-        if (C[i + 1] == '.') {
-            iD = i + 1;
-        }
-    }
-    iR = i;
-    i--;
-
-    for (; (i != iD) && iD != 0; i--) {
-        radius += powf(10, -(i - iD)) * (C[i] - 48);
-    }
-    for (i--; i != iL - 1; i--) {
-        radius += pow(10, (iD - i - 1)) * (C[i] - 48);
-    }
-
-    i = iR;
-
-    for (; C[i] == ' '; i++)
-        ;
-
-    if (C[i] == ')') {
-        i++;
-    } else {
-        printf("closing brackets are not correct");
-        return 3;
-    }
-
-    while (C[i] != EOF) {
-        printf("%c", C[i]);
-        i++;
-    }
-
-    printf("%s(%f %f, %f)\n", circleName, x_circle, y_circle, radius);
-    printf("Circle perimeter - %f, Area - %f",
-           OutputCirclePerim(radius),
-           OutputCircleArea(radius));
-
-    return 0;
+    }	
 }
+
+float ReadNum(char *&C, char *temp, int &i){
+	
+}
+
+int ProcessLine(char *&C, float *coords, int lineAmount){
+	int i=0;
+	
+	char temp[20];
+	int j;
+	SkipSpaces(C,i);
+	
+	if(CheckWord(C,i)){
+		return 1;
+	}
+	
+	SkipSpaces(C,i);
+	
+	if(C[i]!='(')
+		return 2;
+	
+	SkipSpaces(C,i);
+	
+	coords[0]=ReadNum(C, temp, i);
+	
+	printf("C[%d]=%c",i, C[i]);
+	
+	return 0;
+}
+
+//'0'=48 '9'=57
+int ProcessGeomFile()
+{
+	int i;
+	FILE* file;
+	char fname[] = "lab_input.txt";
+	file = fopen(fname, "r");
+	
+	char **C;
+	
+	int lineAmount = CountLines(file);
+
+	AllocateMemory(C, lineAmount);
+	
+	ReadLines(file, C, lineAmount);
+	
+	float coords[lineAmount][3]; //every line of this array contains x,y,radius of every circle
+	
+	for(i=0;i<lineAmount;i++){
+		switch(ProcessLine(C[i], coords[i], lineAmount)){
+			case 1:
+				printf("word circle is not correct\n");
+				return 1;
+			case 2:
+				printf("expected opening bracket\n");
+				return 2;
+			default:
+				break;
+		}
+	}
+	return 0;
+}
+
--- src/libgeometry/geom_string.h
+++ src/libgeometry/geom_string.h
@@ -1,9 +1,21 @@
 #pragma once
 
-int ProcessGeomFile();
-
 float OutputCirclePerim();
 
 float OutputCircleArea();
 
+int ProcessGeomFile();
+
 void SkipSpaces();
+
+int CountLines();
+
+void AllocateMemory();
+
+void ReadLines();
+
+int CheckWord();
+
+float ReadNum();
+
+int ProcessLine();
