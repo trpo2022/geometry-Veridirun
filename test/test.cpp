d5bfa85380df63d9a1920c2620289203a3edcaf8
Kirill Gaponov <28047836+Veridirun@users.noreply.github.com>
2022-05-13 20:03:59 +0700

    Change paths to input files

--- test/test.cpp
+++ test/test.cpp
@@ -3,42 +3,42 @@
 
 CTEST(suite, test1_true)
 {
-    int result = ProcessGeomFile("../test/testfiles/test1.txt");
+    int result = ProcessGeomFile("test/testfiles/test1.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test2_true)
 {
-    int result = ProcessGeomFile("../test/testfiles/test2.txt");
+    int result = ProcessGeomFile("test/testfiles/test2.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test3_true)
 {
-    int result = ProcessGeomFile("../test/testfiles/test3.txt");
+    int result = ProcessGeomFile("test/testfiles/test3.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test4_true)
 {
-    int result = ProcessGeomFile("../test/testfiles/test4.txt");
+    int result = ProcessGeomFile("test/testfiles/test4.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test5_true)
 {
-    int result = ProcessGeomFile("../test/testfiles/test5.txt");
+    int result = ProcessGeomFile("test/testfiles/test5.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test6_circleword)
 {
-    const int result = ProcessGeomFile("../test/testfiles/test6.txt");
+    const int result = ProcessGeomFile("test/testfiles/test6.txt");
 
     const int expected = 1;
 
@@ -47,14 +47,14 @@ CTEST(suite, test6_circleword)
 
 CTEST(suite, test7_lotsofspaces)
 {
-    const int result = ProcessGeomFile("../test/testfiles/test7.txt");
+    const int result = ProcessGeomFile("test/testfiles/test7.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test8_badbracket)
 {
-    int result = ProcessGeomFile("../test/testfiles/test8.txt");
+    int result = ProcessGeomFile("test/testfiles/test8.txt");
 
     const int expected = 3;
 
@@ -63,7 +63,7 @@ CTEST(suite, test8_badbracket)
 
 CTEST(suite, test9_badnum)
 {
-    int result = ProcessGeomFile("../test/testfiles/test9.txt");
+    int result = ProcessGeomFile("test/testfiles/test9.txt");
 
     const int expected = 3;
 
@@ -72,7 +72,7 @@ CTEST(suite, test9_badnum)
 
 CTEST(suite, test10_missingcomma)
 {
-    int result = ProcessGeomFile("../test/testfiles/test10.txt");
+    int result = ProcessGeomFile("test/testfiles/test10.txt");
 
     const int expected = 4;
 
@@ -81,7 +81,7 @@ CTEST(suite, test10_missingcomma)
 
 CTEST(suite, test11_badnum)
 {
-    int result = ProcessGeomFile("../test/testfiles/test11.txt");
+    int result = ProcessGeomFile("test/testfiles/test11.txt");
 
     const int expected = 3;
 
@@ -90,7 +90,7 @@ CTEST(suite, test11_badnum)
 
 CTEST(suite, test12_missingleftbracket)
 {
-    int result = ProcessGeomFile("../test/testfiles/test12.txt");
+    int result = ProcessGeomFile("test/testfiles/test12.txt");
 
     const int expected = 2;
 
@@ -99,7 +99,7 @@ CTEST(suite, test12_missingleftbracket)
 
 CTEST(suite, test13_missingrightbracket)
 {
-    int result = ProcessGeomFile("../test/testfiles/test13.txt");
+    int result = ProcessGeomFile("test/testfiles/test13.txt");
 
     const int expected = 5;
 
